960972a23e381f16b69ce4f56288a975329e2088
Tigerly1 <tigerlymad@gmail.com>
2023-10-10 01:06:38 +0200

    Added fetching movies on backend

---
+++ src/app/api/movies/route.ts
@@ -0,0 +1,33 @@
+import axios, { all } from "axios";
+import { NextResponse } from "next/server";
+
+interface Movie {
+  id: number;
+  title: string;
+  poster_path: string;
+  vote_average: number;
+  release_date: string;
+}
+
+const API_KEY = process.env.MOVIES_API_KEY;
+const BASE_URL = "https://api.themoviedb.org/3";
+// https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const sort = searchParams.get("sort") ?? "DESC";
+  const index = Number(searchParams.get("index") ?? 0);
+  const sortOrder = sort === "DESC" ? "desc" : "asc";
+  try {
+
+    const results = await axios.get(
+      `${BASE_URL}/discover/movie?include_adult=false&include_video=false&language=en-US&without_genres=99,10755&vote_count.gte=200&api_key=${API_KEY}&page=${
+        index + 1
+      }&sort_by=vote_average.${sortOrder}`
+    );
+    const Movies: Movie[] = results.data.results
+    return NextResponse.json(Movies);
+  } catch (error: any) {
+    console.error("Error fetching movies:", error.message);
+    return NextResponse.json("error", { status: 400 });
+  }
+}
